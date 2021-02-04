feb3b6575f67f34bab9bac0f1693c667a9b1766f
Eugene Maltsev <no.one.left.disappointed@gmail.com>
2021-02-04 17:07:31 +0200

    Initial Commit

---
+++ UIKit_Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Training
+//
+//  Created by Eugene Maltsev on 04.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
